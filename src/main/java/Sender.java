802f6a32a2ead57e8d7a983694ae68ef4b683632
ВашеEvgenyKuzingit config --global user.email Ваш < evgenykuzin21@gmail.com>
2020-07-15 10:23:41 +0300

    fix multi messages

--- src/main/java/Sender.java
+++ src/main/java/Sender.java
@@ -6,17 +6,26 @@ import java.util.Locale;
 
 public class Sender extends Thread {
     private Bot bot;
+
     public Sender(Bot bot) {
         this.bot = bot;
     }
+
     private boolean needToSendWednesday = Context.needToSendWednesday;
+
     @Override
     public void run() {
+        long lastCall = 0;
         while (true) {
-            String date = new SimpleDateFormat("EEEE", new Locale("en")).format(new Date());
-            if (date.toLowerCase().equals("wednesday") && needToSendWednesday) {
-                bot.sendPhoto(Context.SISKAPISKA_CHAT_ID, Context.WEDNESDAY_PHOTO_ID);
-                needToSendWednesday = false;
+            if (System.currentTimeMillis() - lastCall > 60000) {
+                lastCall = System.currentTimeMillis();
+                String date = new SimpleDateFormat("EEEE", new Locale("en")).format(new Date());
+                String time = new SimpleDateFormat("hh:mm").format(new Date());
+                boolean isTimeToSend = date.toLowerCase().equals("wednesday") && (time.equals("10:30") || time.equals("12:30"));
+                if (isTimeToSend && needToSendWednesday) {
+                    bot.sendPhoto(Context.SISKAPISKA_CHAT_ID, Context.WEDNESDAY_PHOTO_ID);
+                    needToSendWednesday = false;
+                }
             } else needToSendWednesday = true;
         }
     }
